25b5398b179c516329b7ef580d6c8b8693b5f2d5
Katherine445 <xollllera@gmail.com>
2016-07-28 16:19:12 +0300

    fixed bugs with autocomplete

--- public/js/components/CommandLine.jsx
+++ public/js/components/CommandLine.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import {getCommandHint} from '../lib/hints';
 import RawHtml from './RawHtml';
-import {getHint, validateCommand, execCommand} from '../lib/commands';
+import {getHint, validateCommand, execCommand, getAutocomplete} from '../lib/commands';
 import {selectLastTodo} from '../actions/todos';
 import {selectedCommandLine} from '../actions/app';
 
@@ -25,54 +25,9 @@ export default class CommandLine extends Component {
         break;
       case 'Tab':
         /**
-         * --------------
-         * trying to write autocomplete (by Katherine)
-         * --------------
+         * autocomplete
          */
-        var command = validateCommand(this.refs.ctrlInput.value, this.context.store.getState().todos); // getting all matches
-
-        console.log(command);
-        
-        /**
-         * find only 1 match
-         */
-        if(command.length == 1){	  
-          this.refs.ctrlInput.value = command[0].action + " ";	  // do autocomplete
-        }else{
-        console.log('here is lot more then one match');
-        /**
-         * found more then 1 match
-         */ 
-          if(command.length > 0){
-            /**
-             * get list of first words function names 
-             */
-            var firstWords = command.map(function(str, index, array){
-              /**
-               * get function name      
-               */
-              var fname = str.action;
-              /**
-               * get first word of camelCase function name
-               */
-              var matches = fname.substring(0,fname.indexOf(fname.match(/[A-Z]/)));
-
-              return matches;
-            });
-            var compares = compareArray(firstWords);
-
-            if (compares && this.refs.ctrlInput.value <= compares){
-              this.refs.ctrlInput.value = compares;
-            }else{
-              /**
-               * do nothing, waiting for input
-               */
-            }
-          }
-        }
-         /**
-         * ---------------
-         */
+        this.refs.ctrlInput.value = getAutocomplete(this.refs.ctrlInput.value, this.context.store.getState().todos);
 
         stopEvent(e);
         break;
@@ -137,42 +92,3 @@ function stopEvent(e) {
   e.stopPropagation();
   e.preventDefault();
 }
-
-/**
- * written by katherine 27.07
-  * function of finding equals in array
-  */
-  function compareArray(arr){
-    var result, eqv;
-    /**
-     * find matches
-     * if all words in array is equal, return word
-     * else return undefined
-     */
-    
-    /**
-     * fix this curve hands pls =)
-     */
-    for (var i = 0; i < arr.length; i++){
-      var compare = arr[i];
-      
-      for(var j=i+1; j < arr.length; j++){
-        if(arr[j] == compare){
-          eqv = true;
-        }
-         else
-          eqv = false;
-      }
-    }
-    //console.log("eqv = " + eqv);
-      if (eqv == true){
-        result = arr[0];
-      }
-      console.log(result);
-
-    return result;
-  }
-
-/**
-  * ---------**
-  */
